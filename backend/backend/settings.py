91ab8bcc0658acb24f9ce72945bf6f8de0729f50
stepaxvii <153988379+stepaxvii@users.noreply.github.com>
2024-09-11 15:55:45 +0300

    Add setup.cfg

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = [ '127.0.0.1', 'localhost', 'igorstepa.hopto.org', '89.169.163.213']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'igorstepa.hopto.org', '89.169.163.213']
 
 
 # Application definition
